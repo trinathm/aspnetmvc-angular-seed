1e2296bc7788d4c526247c5e80a3cd1babcdfacb
Trinath Mallavarapu <trinathm@outlook.com>
2017-11-12 01:25:17 +0530

    migrated service to httpclient from http

--- AngularSeed/AngularSeed.Web/Scripts/app/dashboard/dashboard.service.ts
+++ AngularSeed/AngularSeed.Web/Scripts/app/dashboard/dashboard.service.ts
@@ -1,11 +1,14 @@
 ﻿import { Injectable } from '@angular/core';
-import { Headers, Http } from '@angular/http';
-
-import 'rxjs/add/operator/toPromise';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class DashboardService {
     private headers = new Headers({ 'Content-Type': 'application/json' });
 
-    constructor(private http: Http) { }
+    constructor(private http: HttpClient) { }
+
+    get(): Observable<any> {
+        return this.http.get('/api/dashboard');
+    }
 }
